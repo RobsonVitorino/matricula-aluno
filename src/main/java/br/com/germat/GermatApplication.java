a339c0d7c877c127fffaa0e3503968c12f7562fb
RobsonVitorino <robson.jr1@hotmail.com>
2024-01-15 20:32:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/germat/GermatApplication.java
@@ -0,0 +1,13 @@
+package br.com.germat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/germat/GermatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.germat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
