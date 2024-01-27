94aa6bde36c4362698c0635d3d5869c21f70e41c
RobsonVitorino <robson.jr1@hotmail.com>
2024-01-27 09:09:45 -0300

    Correção no erro de versão da dependência/MySQL no pom.xml

--- src/main/java/br/com/germat/model/Aluno.java
+++ src/main/java/br/com/germat/model/Aluno.java
@@ -1,5 +1,15 @@
 package br.com.germat.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 @Entity
 public class Aluno {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
 }
